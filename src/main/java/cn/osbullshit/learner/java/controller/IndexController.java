410c3ee0f0ca1180fcf8a52590143ef3d853aa64
baoqizhang <402234435.lz@gmail.com>
2017-06-07 13:08:04 +0800

    测试

---
+++ src/main/java/cn/osbullshit/learner/java/controller/IndexController.java
@@ -0,0 +1,25 @@
+/** 
+ * Project Name:Learner 
+ * File Name:IndexController.java 
+ * Package Name:cn.osbullshit.learner.java.controller 
+ * Date:2017年6月7日下午1:06:54 
+ * 
+ */  
+  
+package cn.osbullshit.learner.java.controller;  
+/** 
+ * ClassName:IndexController 
+ * Function: TODO ADD FUNCTION. 
+ * Date:     2017年6月7日 下午1:06:54 
+ * @author   baoqi.zhang 
+ * @version   
+ * @since    JDK 1.7 
+ * @see       
+ */
+public class IndexController {
+
+	public static void main(String[] args) {
+		
+	}
+}
+ 
