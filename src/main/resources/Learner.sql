f1858fb7b4e846ab5e37c33c2f02a51cd5cebf3a
zlmismeno1 <baoqizhang666@163.com>
2017-06-07 21:02:24 +0800

    试题以及答案 sql语句

--- src/main/resources/Learner.sql
+++ src/main/resources/Learner.sql
@@ -1,3 +1,5 @@
+create database osbullshit_Learner;
+use osbullshit_Learner;
 /* 用户表 */
 create table tb_learner_user(
 	id varchar(30) primary key,
@@ -10,3 +12,14 @@ insert into tb_learner_user values ("c","c","c");
 insert into tb_learner_user values ("d","中文测试","中文测试");
 
 select * from tb_learner_user;
+
+/*试题以及答案*/
+create table tb_learner_qAndA (
+	id varchar(30) primary key,
+	userId varchar(30) ,
+	title varchar(255) not null,
+	question varchar(255) not null,
+	answer varchar(300) not null,
+	isDelete int not null,
+	update_time TIMESTAMP 
+);
