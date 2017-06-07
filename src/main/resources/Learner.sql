4e6f6612bd6e0598d6e63a4da6237dc0daf2c89a
baoqizhang <Dell@Dell-PC>
2017-06-07 12:03:41 +0800

    初始导入

---
+++ src/main/resources/Learner.sql
@@ -0,0 +1,9 @@
+create table test(
+	id varchar(30) primary key,
+	age int ,
+	name varchar(255)
+);
+insert into test values('adfad',11,'bob');
+insert into test values('adsf',11,'波波');
+
+select * from test
