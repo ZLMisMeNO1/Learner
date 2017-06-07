b1767abad34b643ca71f42cea1349bddfded1f10
zlmismeno1 <baoqizhang666@163.com>
2017-06-07 21:12:02 +0800

    试题以及答案数据结构修改，id为自增数字

--- src/main/resources/Learner.sql
+++ src/main/resources/Learner.sql
@@ -15,7 +15,7 @@ select * from tb_learner_user;
 
 /*试题以及答案*/
 create table tb_learner_qAndA (
-	id varchar(30) primary key,
+	id int auto_increment primary key,
 	userId varchar(30) ,
 	title varchar(255) not null,
 	question varchar(255) not null,
@@ -23,3 +23,12 @@ create table tb_learner_qAndA (
 	isDelete int not null,
 	update_time TIMESTAMP 
 );
+select * from  tb_learner_qAndA;
+insert into tb_learner_qAndA(title,question,answer,isDelete)
+	values ("String,StringBuffer, StringBuilder 的区别是什么","String,StringBuffer, StringBuilder 的区别是什么？String为什么是不可变的？","1、String是字符串常量，StringBuffer和StringBuilder都是字符串变量。后两者的字符内容可变，而前者创建后内容不可变。
+
+2、String不可变是因为在JDK中String类被声明为一个final类。
+
+3、StringBuffer是线程安全的，而StringBuilder是非线程安全的。
+
+ps：线程安全会带来额外的系统开销，所以StringBuilder的效率比StringBuffer高。如果对系统中的线程是否安全很掌握，可用StringBuffer，在线程不安全处加上关键字Synchronize。",0)
