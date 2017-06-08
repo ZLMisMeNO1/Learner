e9a79ea5654ec94456a21d78f9e2a122c44fb414
baoqi.zhang <baoqizhang666@163.com>
2017-06-08 18:39:34 +0800

    试题添加所属语言属性

--- src/main/resources/Learner.sql
+++ src/main/resources/Learner.sql
@@ -21,16 +21,18 @@ create table tb_learner_qAndA (
 	question varchar(255) not null,
 	answer varchar(300) not null,
 	isDelete int not null,
-	update_time TIMESTAMP 
+	update_time TIMESTAMP ,
+	language varchar(30) not null
 );
+drop table tb_learner_qAndA;
 select * from  tb_learner_qAndA;
-insert into tb_learner_qAndA(title,question,answer,isDelete)
+insert into tb_learner_qAndA(title,question,answer,isDelete,language)
 	values ("String,StringBuffer, StringBuilder 的区别是什么","String,StringBuffer, StringBuilder 的区别是什么？String为什么是不可变的？","1、String是字符串常量，StringBuffer和StringBuilder都是字符串变量。后两者的字符内容可变，而前者创建后内容不可变。
 
 2、String不可变是因为在JDK中String类被声明为一个final类。
 
 3、StringBuffer是线程安全的，而StringBuilder是非线程安全的。
 
-ps：线程安全会带来额外的系统开销，所以StringBuilder的效率比StringBuffer高。如果对系统中的线程是否安全很掌握，可用StringBuffer，在线程不安全处加上关键字Synchronize。",0);
-insert into tb_learner_qAndA(title,question,answer,isDelete)
-	values ("什么是认知？","认知是什么?","认知是一个过程",0)
+ps：线程安全会带来额外的系统开销，所以StringBuilder的效率比StringBuffer高。如果对系统中的线程是否安全很掌握，可用StringBuffer，在线程不安全处加上关键字Synchronize。",0,"java");
+insert into tb_learner_qAndA(title,question,answer,isDelete,language)
+	values ("什么是认知？","认知是什么?","认知是一个过程",0,"java")
