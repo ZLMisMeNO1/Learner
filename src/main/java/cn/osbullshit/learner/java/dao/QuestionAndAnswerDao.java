d2b3384de11500ddf9d0e8e879cb9332e5b700cc
baoqi.zhang <baoqizhang666@163.com>
2017-06-08 19:05:55 +0800

    新增分页加载试题dao层接口 新增获取当前试题id值最大值dao层接口

--- src/main/java/cn/osbullshit/learner/java/dao/QuestionAndAnswerDao.java
+++ src/main/java/cn/osbullshit/learner/java/dao/QuestionAndAnswerDao.java
@@ -1,17 +1,41 @@
 package cn.osbullshit.learner.java.dao;
 
+import java.util.List;
+import java.util.Map;
+
 import cn.osbullshit.learner.java.pojo.QuestionAndAnswerBean;
 
 public interface QuestionAndAnswerDao {
 	/**
 	 * 
-	 * @Title: getQandA   获取试题
-	 * @Description: 
-	 * @param: @param id
-	 * @param: @return      
-	 * @return: QuestionAndAnswerBean      
-	 * @throws   
-	 * @author baoqi.zhang
+	 * listQuestionAndAnswers:分页加载试题
+	 * 
+	 * @author baoqi.zhang 
+	 * @param params
+	 * @return 
+	 * @since JDK 1.7
 	 */
-	QuestionAndAnswerBean getQandA(Integer id);
+	List<QuestionAndAnswerBean> listQuestionAndAnswers(Map<String,Object> params);
+	
+	/**
+	 * 
+	 * getQuestionAndAnswer:获取单个试题
+	 * 
+	 * @author baoqi.zhang 
+	 * @param id
+	 * @return 
+	 * @since JDK 1.7
+	 */
+	QuestionAndAnswerBean getQuestionAndAnswer(Integer id);
+	
+	/**
+	 * 
+	 * getMaxId:获取当前试题id的最大值，
+	 * 在增加试题时，应该使id值连续
+	 * 
+	 * @author baoqi.zhang 
+	 * @return 
+	 * @since JDK 1.7
+	 */
+	Integer getMaxId();
 }
